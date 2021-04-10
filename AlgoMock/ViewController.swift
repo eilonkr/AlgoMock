3b1364bd51efd8981e92a20ec5273470b859fd86
Eilon Krauthammer <eilon@socialkit.com>
2021-04-10 16:03:06 +0300

    Initial Commit

---
+++ AlgoMock/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AlgoMock
+//
+//  Created by Eilon Krauthammer on 10/04/2021.
+//
+
+import UIKit
+
+protocol Hello {
+    
+}
+
+protocol World {
+    
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+
+}
+
